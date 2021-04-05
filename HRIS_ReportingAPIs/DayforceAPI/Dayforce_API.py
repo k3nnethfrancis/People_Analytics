7add56e7b40150020e9a778a1add2d9885b74c51
Ken Cavanagh <36281899+kfcav@users.noreply.github.com>
2021-04-05 10:33:27 -0700

    Add files via upload

---
+++ HRIS_ReportingAPIs/DayforceAPI/Dayforce_API.py
@@ -0,0 +1,41 @@
+#!python3 
+
+# import modules
+import os
+import json
+import requests
+import pandas as pd
+
+def DF_api(report_name):
+    '''
+    Takes a report integration name report_name, sends a get request, parses json, and returns a dataframe.
+        Parameters:
+                report_name: report integration name 
+        Returns:
+            dataframe of said report.
+        Notes:
+            Dayforce report must be a V2 report.
+            Row Limit: 5000
+    '''
+    # get credentials from environment variables
+    USER = os.environ['DF_USER']
+    PASS = os.environ['DF_PASS']
+
+    report = report_name
+
+    url = r"https://usr58-services.dayforcehcm.com/Api/Motivate/V1/Reports/" + report
+
+    # perform get request to obtain json data
+
+    r = requests.get(url , auth=(USER, PASS))
+
+    # convert json to a datafram
+
+    r_json = r.json()
+    df = pd.DataFrame.from_dict(r_json['Data']['Rows'])
+    return df
+
+my_report = DF_api("report_integration_name")
+
+# Optional: Write the DataFrame to a .csv file
+#my_report.to_csv(r"C:\Users\PATH...\Desktop\FileName.csv", index=False)
